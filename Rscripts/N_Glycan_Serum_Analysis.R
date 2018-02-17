561b90f97329bf7037896f2a720695958db25b72
Elena Papaleo <ele@Elenas-MacBook-Pro.local>
2018-02-17 22:22:59 +0100

    corrections autorship comments in the scripts - autorship included in the DESCRIPTION

--- Rscripts/N_Glycan_Serum_Analysis.R
+++ Rscripts/N_Glycan_Serum_Analysis.R
@@ -1,12 +1,3 @@
-
-# Author: Thilde Bagger Terkelsen
-# Contact: thilde@cancer.dk
-# Place of employment: Computational Biology Laboratory, Danish Cancer Society Research Center, Copenhagen, Denmark
-# Date 29-05-2017
-
-
-
-
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
